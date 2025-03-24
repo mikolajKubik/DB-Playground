6c15f7e83efbd48ddbeba2d43aa88d9e92ab8245
kamil <kamildzierzawskI@gmail.com>
2025-03-24 20:04:54 +0100

    Code clean up

--- consumer/src/main/java/edu/kdmk/repository/RentRepository.java
+++ consumer/src/main/java/edu/kdmk/repository/RentRepository.java
@@ -1,19 +1,12 @@
 package edu.kdmk.repository;
 
-import com.mongodb.client.ClientSession;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import edu.kdmk.model.Rent;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import static com.mongodb.client.model.Filters.eq;
 
 public class RentRepository {
     private final MongoCollection<Rent> rentCollection;
